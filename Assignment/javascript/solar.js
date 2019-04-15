bcaccaeecee8b0878d0d133897940da2edad9344
SakuraRhymes <sakurarhymes@gmail.com>
2019-04-15 18:02:09 +0800

    Improved js

--- Assignment/javascript/solar.js
+++ Assignment/javascript/solar.js
@@ -1,12 +1,3 @@
 function onHoverChangeiframe(planetName) {
-    var iframe = document.querySelector("#iframe_planet");
-    switch (planetName) {
-        case "Mercury":
-            iframe.src = "mercury.html";
-            break;
-        case "Venus":
-            iframe.src = "venus.html";
-            break;
-    }
+    document.querySelector("#iframe_planet").src = planetName + ".html";
 }
-
